b6684b787633ad04c407d5b02765f4c7e71ef838
Min-92 <0201.mj.lee@gmail.com>
2019-07-18 21:14:13 +0900

    feat : 회원가입 페이지 추가

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const path = require('path');
+const logger = require('morgan');
 
 const loginRouter = require('./routes/login');
+const signupRouter = require('./routes/signup');
 
 const port = 3000;
 
@@ -10,9 +12,12 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(logger('dev'));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/login', loginRouter);
+app.use('/signup', signupRouter);
 
 
 app.listen(port,() =>{
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,23 +1,23 @@
-class Login {
+class Signup {
     constructor() {
         this.body = document.querySelector('body');
         this.main = document.querySelector('main');
         this.header;
-        this.loginBox;
+        this.signupBox;
     }
 
     setPage() {
         this.body.insertBefore(this.setHeader(), this.main);
-        this.main.appendChild(this.setLoginBox());
+        this.main.appendChild(this.setSignupBox());
     }
 
-    setLoginBox(){
-        const loginForm = document.createElement('form');
+    setSignupBox(){
+        const signupForm = document.createElement('form');
         const idTextArea = document.createElement('input');
         const pwTextArea = document.createElement('input');
         const button = document.createElement('input');
-        loginForm.setAttribute('action','/login');
-        loginForm.setAttribute('method','POST');
+        signupForm.setAttribute('action','/signup');
+        signupForm.setAttribute('method','POST');
         idTextArea.setAttribute('name', 'userID');
         idTextArea.setAttribute('type', 'text');
         idTextArea.placeholder = 'Please input your ID';
@@ -25,17 +25,17 @@ class Login {
         pwTextArea.setAttribute('type', 'password');
         pwTextArea.placeholder = 'Please input your PW';
         button.setAttribute('type', 'submit');
-        button.setAttribute('value', 'Log in');
-        loginForm.appendChild(idTextArea);
-        loginForm.appendChild(pwTextArea);
-        loginForm.appendChild(button);
-        return loginForm;
+        button.setAttribute('value', 'Sign up');
+        signupForm.appendChild(idTextArea);
+        signupForm.appendChild(pwTextArea);
+        signupForm.appendChild(button);
+        return signupForm;
     }
 
     setHeader() {
         this.header = document.createElement('header');
         const title = document.createElement('div');
-        title.innerText = 'Log In';
+        title.innerText = 'Sign up';
         this.header.appendChild(title);
         this.header.appendChild(this.setSignUpButton());
         return this.header;
@@ -43,16 +43,16 @@ class Login {
 
     setSignUpButton() {
         const signUpForm = document.createElement('form');
-        signUpForm.setAttribute('action', '/signup');
+        signUpForm.setAttribute('action', '/login');
         signUpForm.setAttribute('method', 'GET');
         const button = document.createElement('input');
         button.setAttribute('type', 'submit');
-        button.setAttribute('value', 'Sign Up');
+        button.setAttribute('value', 'Log in');
         signUpForm.appendChild(button);
         return signUpForm;
     }
 }
 
-const login = new Login();
+const signUp = new Signup();
 
-login.setPage();
+signUp.setPage();
