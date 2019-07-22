c4a2c5ad72e0738c6f47e3dcdfa177cdb417699e
Min-92 <0201.mj.lee@gmail.com>
2019-07-22 20:24:34 +0900

    feat : 에러 검증 추가
    - error.pug 추가
    - style.cs 추가

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
+const session = require('express-session');
+const mongoose = require('mongoose');
 const express = require('express');
-const path = require('path');
 const logger = require('morgan');
+const path = require('path');
+const passport = require('passport');
+const Strategy = require('passport-local').Strategy;
 
+const passportConfig = require('./passport/index');
 const loginRouter = require('./routes/login');
 const signupRouter = require('./routes/signup');
 
 const port = 3000;
+const mongoURI = 'mongodb://localhost/news';
 
 const app = express();
+
+passportConfig(passport);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -16,10 +24,35 @@ app.use(logger('dev'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(session({
+    secret: '#newssecret',
+    resave: false,
+    saveUninitialized: true
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 app.use('/login', loginRouter);
 app.use('/signup', signupRouter);
 
+app.use(function (req, res, next) {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+app.use(function (err, req, res, next) {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.status(err.status || 500);
+    res.render('error');
+})
 
-app.listen(port,() =>{
+mongoose.connect((mongoURI), { useNewUrlParser: true });
+
+app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
