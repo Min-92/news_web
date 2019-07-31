d1490218d5eda0f9d0d287670f38660154152f1c
Min-92 <0201.mj.lee@gmail.com>
2019-08-01 03:20:52 +0900

    feat : article schema 추가

--- models/database.js
+++ models/database.js
@@ -4,19 +4,24 @@ const mongoURI = 'mongodb://localhost/news';
 mongoose.connect((mongoURI), { useNewUrlParser: true });
 const db = mongoose.connection;
 
-db.once('open', () => {
-    console.log('mongodb connected');
-
-});
-
 const userSchema = new mongoose.Schema({
     id: { type: String, required: true, unique: true },
     pw: { type: String, required: true },
-    date: { type: String, default: Date.now },
     authority: { type: String, default: "normal" }
-})
+},{ timestamps: { createdAt: 'created_at' }})
 
 const User = mongoose.model('User', userSchema);
 
+const articleSchema = new mongoose.Schema({
+    title: { type: String, required : true},
+    number : {type : Number, required: true},
+    main: {type: String, required: true},
+    img : {type:String},
+    comments : [{comment : String, user : String}],
+    like : [{user : String}],
+    hidden : {type : Boolean, default: false}
+}, { timestamps: { createdAt: 'created_at' }})
 
-module.exports = { db, User };
+const Article = new mongoose.model('Article', articleSchema);
+
+module.exports = { db, User, Article };
