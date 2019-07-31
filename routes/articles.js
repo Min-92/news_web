96ba798bf11335f3d0de34563c291d3ae5cc408b
Min-92 <0201.mj.lee@gmail.com>
2019-07-31 14:00:56 +0900

    feat : 메인(기사리스트) 페이지 추가

--- routes/articles.js
+++ routes/articles.js
@@ -4,7 +4,19 @@ const router = express.Router();
 const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
 
 router.get('/', (req, res, next) => {
-    res.render('articles');
+    const pageNumber = req.query.page || 1;
+    const skipCount = 10*(pageNumber-1);
+    const user = req.isAuthenticated() ? req.session.passport.user : undefined;
+
+    Article.find({hidden : false}).sort({number : -1}).skip(skipCount).limit(10).exec((err, articles) => {
+        if(articles.length === 0) next(err);
+        res.render('articles', {
+            title : "News",
+            articles,
+            user
+        });
+    });
+
 });
 
 router.get('/:number', async (req, res, next) => {
@@ -14,12 +26,13 @@ router.get('/:number', async (req, res, next) => {
     if (!article) next();
     const user = req.isAuthenticated() ? req.session.passport.user : undefined;
     res.render('article', { 
+        number,
         title: "News",
         articleTitle : article.title, 
         img : article.img, 
         mainText : article.main, 
         comments : article.comments, 
-        actionURL : `/articles/comment/${number}`, 
+        like : article.like.length,
         user
     });
 
@@ -34,7 +47,6 @@ router.post('/comment/:number', isLoggedIn, async (req, res, next) => {
     await article.save();
     res.redirect(`/articles/${number}`);
 });
-
 
 module.exports = router;
 
