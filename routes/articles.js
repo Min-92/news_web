8d69f4bb6b2713e3f3ea972a5b265b5adf84cf90
Min-92 <0201.mj.lee@gmail.com>
2019-08-01 03:26:00 +0900

    feat : 메인(기사리스트)페이지, 기사 상세 페이지 구현
    - 좋아요 구현
    - 댓글 구현
    - 페이지네이션 구현

--- routes/articles.js
+++ routes/articles.js
@@ -1,20 +1,36 @@
 const express = require('express');
 const { Article } = require('../models/database');
 const router = express.Router();
-const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
+const { isLoggedIn } = require('./middlewares');
+const { getMessage } = require('../controller');
 
-router.get('/', (req, res, next) => {
+const numberOfArticlesInPage = 6;
+
+router.get('/', async (req, res, next) => {
     const pageNumber = req.query.page || 1;
-    const skipCount = 10*(pageNumber-1);
+    const skipCount = numberOfArticlesInPage * (pageNumber - 1);
     const user = req.isAuthenticated() ? req.session.passport.user : undefined;
 
-    Article.find({hidden : false}).sort({number : -1}).skip(skipCount).limit(10).exec((err, articles) => {
-        if(articles.length === 0) next(err);
-        res.render('articles', {
-            title : "News",
-            articles,
-            user
-        });
+    const articles = await Article.find({ hidden: false }).sort({ number: -1 });
+
+    const articlesInPage = articles.reduce((acc, value, index) => {
+        if (index < skipCount) return acc;
+        if (index >= skipCount + numberOfArticlesInPage) return acc;
+        return [...acc, value];
+    }, [])
+
+    const maxPage = Math.ceil(articles.length / numberOfArticlesInPage)
+    const pages = [];
+    for (let i = 1; i <= maxPage; i++) {
+        pages.push(i);
+    }
+
+    res.render('articles', {
+        title: "News",
+        articlesInPage,
+        user,
+        pageNumber,
+        pages
     });
 
 });
@@ -25,40 +41,49 @@ router.get('/:number', async (req, res, next) => {
 
     if (!article) next();
     const user = req.isAuthenticated() ? req.session.passport.user : undefined;
-    res.render('article', { 
+    const hasLiked = article.like.filter(obj => obj.user === user)[0];
+    const message = getMessage(req);
+
+    res.render('article', {
         number,
         title: "News",
-        articleTitle : article.title, 
-        img : article.img, 
-        mainText : article.main, 
-        comments : article.comments, 
-        like : article.like.length,
-        user
+        articleTitle: article.title,
+        img: article.img,
+        mainText: article.main,
+        comments: article.comments,
+        like: article.like.length,
+        user,
+        message,
+        hasLiked
     });
 
 });
 
 router.post('/comment/:number', isLoggedIn, async (req, res, next) => {
     const number = req.params.number;
-    const {comment} = req.body;
+    const { comment } = req.body;
     const user = req.session.passport.user;
     const article = await Article.findOne({ number });
-    await article.comments.push({user,comment});
+
+    await article.comments.push({ user, comment });
     await article.save();
+
     res.redirect(`/articles/${number}`);
 });
 
-router.post('/like/:number', isLoggedIn, async (req, res, next) => {
+router.post('/like/:number', isLoggedIn, async (req, res) => {
     const number = req.params.number;
     const user = req.session.passport.user;
     const article = await Article.findOne({ number });
     const hasLiked = article.like.filter(obj => obj.user === user)[0];
-    if(!hasLiked) {
-        await article.like.push({user});
-    }else{
+
+    if (hasLiked) {
         await article.like.pull(hasLiked._id);
+    } else {
+        await article.like.push({ user });
     }
     await article.save();
+
     res.redirect(`/articles/${number}`);
 });
 
