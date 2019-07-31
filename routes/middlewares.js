0d889ea4a27a210c7868bf94a728858636b1f0c6
Min-92 <0201.mj.lee@gmail.com>
2019-07-31 16:34:51 +0900

    feat : 로그인 알림 메시지 출력

--- routes/middlewares.js
+++ routes/middlewares.js
@@ -2,7 +2,8 @@ exports.isLoggedIn = (req, res, next) => {
     if(req.isAuthenticated()){
         next();
     }else {
-        res.redirect('/');
+        req.flash('error', '로그인이 필요합니다.');
+        res.redirect('/articles/'+req.params.number);
     }
 }
 
