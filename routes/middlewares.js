ef5a704c3f0ca7891d86096a22e23af930bf2081
Min-92 <0201.mj.lee@gmail.com>
2019-08-01 17:55:04 +0900

    refactor : isLoggedIn 미들웨어 변경
    - 리다이렉트 조건 추가

--- routes/middlewares.js
+++ routes/middlewares.js
@@ -1,11 +1,16 @@
-const User = require('../models/database');
+const {User} = require('../models/database');
 
 exports.isLoggedIn = (req, res, next) => {
     if(req.isAuthenticated()){
         next();
     }else {
         req.flash('error', '로그인이 필요합니다.');
-        res.redirect('/articles/'+req.params.number);
+        const articleNumber = req.params.number;
+        if(articleNumber){
+            res.redirect('/articles/'+req.params.number);
+        }else{
+            res.redirect('/');
+        }
     }
 }
 
@@ -19,7 +24,7 @@ exports.isNotLoggedIn = (req, res, next) => {
 
 exports.isAdmin = async (req,res,next) => {
     const id = req.session.passport.user;
-    const user = await User.find({ id });
+    const user = await User.findOne({ id });
     if(user.authority === "super") next();
     req.flash('error', '권한이 없습니다.');
     res.redirect('/')
