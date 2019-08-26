0e5561a1b6f8bac897882191403ff59f9e0b0378
Min-92 <0201.mj.lee@gmail.com>
2019-08-26 19:07:06 +0900

    refactor : failMesage 객체 생성

--- controller/authController.js
+++ controller/authController.js
@@ -1,12 +1,18 @@
 const { User } = require('../models/database');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const { getMessage } = require('./');
 
 const jwtSecrete = process.env.JWT_SECRET;
 const saltRounds = 10;
 const cookieMaxAge = 60 * 60 * 24 * 14;
+const failMessage = {
+    emptyID: 'ID field is required',
+    emptyPW: 'PW field is required',
+    wrongID: 'Invalid ID',
+    wrongPW: 'Invalid password'
+};
 
-const { getMessage } = require('./');
 
 const compare = async (pw, hash) => {
     const result = await bcrypt.compare(pw, hash);
@@ -27,12 +33,12 @@ module.exports = {
 
     postLogin: async (req, res, next) => {
         const { id, pw } = req.body;
-        if (!id) return loginFail(req, res, 'ID field is required');
-        if (!pw) return loginFail(req, res, 'PW field is required');
+        if (!id) return loginFail(req, res, failMessage.emptyID);
+        if (!pw) return loginFail(req, res, failMessage.emptyPW);
 
         const user = await User.findOne({ id });
-        if (!user) return loginFail(req, res, 'Invalid ID');
-        if (!await compare(pw, user.pw)) return loginFail(req, res, 'Invalid password');
+        if (!user) return loginFail(req, res, failMessage.wrongID);
+        if (!await compare(pw, user.pw)) return loginFail(req, res, failMessage.wrongPW);
 
         const token = jwt.sign({ id }, jwtSecrete);
         res.cookie('token', token, { path: '/', httpOnly: true, maxAge: cookieMaxAge });
