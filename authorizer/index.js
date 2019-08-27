bb66c955d40eee62cc89807f29635ea3e9e3d62d
Min-92 <0201.mj.lee@gmail.com>
2019-08-27 16:48:51 +0900

    feat : login, logout 메소드 추가

--- authorizer/index.js
+++ authorizer/index.js
@@ -1,9 +1,24 @@
+const jwt = require('jsonwebtoken');
+
+const jwtSecrete = process.env.JWT_SECRET;
+const cookieMaxAge = 60 * 60 * 24 * 14;
+
 class Authorizer {
     initialize(req, res, next) {
         req.isAuthenticated = () => {
-            if(req.decoded) return true;
+            if (req.decoded) return true;
             return false;
-        } 
+        }
+
+        req.login = (id) => {
+            const token = jwt.sign({ id }, jwtSecrete);
+            res.cookie('token', token, { path: '/', httpOnly: true, maxAge: cookieMaxAge });
+        }
+
+        req.logout = () => {
+            res.cookie('token', { path: '/', httpOnly: true, maxAge: 0 });
+        }
+
         return next();
     }
 }
